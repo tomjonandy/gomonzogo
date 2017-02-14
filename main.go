4dc7bdc0b02f7fc9d2c91d9c4ad71beb05bff5db
Thomas Andrew <tja523@york.ac.uk>
2017-02-14 17:51:46 +0000

    Update GetTransactions so it returns an array

--- main.go
+++ main.go
@@ -24,35 +24,37 @@ type AccountsResponse struct {
 	Accounts []Account `json:"accounts"`
 }
 
-type Transactions struct {
-	Transactions []struct {
-		ID          string      `json:"id"`
-		Created     time.Time   `json:"created"`
-		Description string      `json:"description"`
-		Amount      int         `json:"amount"`
-		Currency    string      `json:"currency"`
-		Merchant    interface{} `json:"merchant"`
-		Notes       string      `json:"notes"`
-		Metadata    struct {
-			IsTopup string `json:"is_topup"`
-		} `json:"metadata"`
-		AccountBalance int           `json:"account_balance"`
-		Attachments    []interface{} `json:"attachments"`
-		Category       string        `json:"category"`
-		IsLoad         bool          `json:"is_load"`
-		Settled        string        `json:"settled"`
-		LocalAmount    int           `json:"local_amount"`
-		LocalCurrency  string        `json:"local_currency"`
-		Updated        string        `json:"updated"`
-		AccountID      string        `json:"account_id"`
-		Counterparty   struct {
-		} `json:"counterparty"`
-		Scheme            string `json:"scheme"`
-		DedupeID          string `json:"dedupe_id"`
-		Originator        bool   `json:"originator"`
-		IncludeInSpending bool   `json:"include_in_spending"`
-		DeclineReason     string `json:"decline_reason,omitempty"`
-	} `json:"transactions"`
+type Transaction struct {
+	ID          string      `json:"id"`
+	Created     time.Time   `json:"created"`
+	Description string      `json:"description"`
+	Amount      int         `json:"amount"`
+	Currency    string      `json:"currency"`
+	Merchant    interface{} `json:"merchant"`
+	Notes       string      `json:"notes"`
+	Metadata    struct {
+		IsTopup string `json:"is_topup"`
+	} `json:"metadata"`
+	AccountBalance int           `json:"account_balance"`
+	Attachments    []interface{} `json:"attachments"`
+	Category       string        `json:"category"`
+	IsLoad         bool          `json:"is_load"`
+	Settled        string        `json:"settled"`
+	LocalAmount    int           `json:"local_amount"`
+	LocalCurrency  string        `json:"local_currency"`
+	Updated        string        `json:"updated"`
+	AccountID      string        `json:"account_id"`
+	Counterparty   struct {
+	} `json:"counterparty"`
+	Scheme            string `json:"scheme"`
+	DedupeID          string `json:"dedupe_id"`
+	Originator        bool   `json:"originator"`
+	IncludeInSpending bool   `json:"include_in_spending"`
+	DeclineReason     string `json:"decline_reason,omitempty"`
+}
+
+type TransactionsResponse struct {
+	Transactions []Transaction `json:"transactions"`
 }
 
 func GetAccounts() []Account {
@@ -74,7 +76,7 @@ func GetAccounts() []Account {
 	return body.Accounts
 }
 
-func GetTransactions(accountID string) Transactions {
+func GetTransactions(accountID string) []Transaction {
 	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.monzo.com/transactions?account_id=%s", accountID), nil)
 	req.Header.Add("Authorization", "Bearer "+ACCESSTOKEN)
 
@@ -82,15 +84,15 @@ func GetTransactions(accountID string) Transactions {
 
 	if err != nil {
 		fmt.Println(err)
-		return Transactions{}
+		return []Transaction{}
 	}
 
 	defer res.Body.Close()
 
-	body := Transactions{}
+	body := TransactionsResponse{}
 
 	json.NewDecoder(res.Body).Decode(&body)
-	return body
+	return body.Transactions
 }
 
 func main() {
@@ -103,7 +105,7 @@ func main() {
 	var accountNumber int = 0
 	fmt.Scanln(&accountNumber)
 	transactions := GetTransactions(accounts[accountNumber].ID)
-	for i := 0; i < len(transactions.Transactions); i++ {
-		fmt.Printf("[%d]\t%d\t%s\n", i, transactions.Transactions[i].Amount, transactions.Transactions[i].Description)
+	for i := 0; i < len(transactions); i++ {
+		fmt.Printf("[%d]\t%d\t%s\n", i, transactions[i].Amount, transactions[i].Description)
 	}
 }
